7846380f792b3308591b936ac5c73904ba25b442
Harouna <harounathiam2005@gmail.com>
2024-08-27 22:57:59 -0400

    initial file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+// if # == a multiple of 3, output fizz
+// if # == a multiple of 5, output buzz
+// if # == a multiple of both, output fizzbuzz
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
