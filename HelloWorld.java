76132b313a324903c1aeceb993ba5754aa3080fe
harouna <harounathaim2005@gmail.com>
2024-08-28 01:29:19 -0400

    init from mac

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 public class HelloWorld {
 
-// if # == a multiple of 3, output fizz
-// if # == a multiple of 5, output buzz
-// if # == a multiple of both, output fizzbuzz
-
     public static void main(String[] args) {
+        System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
     }
