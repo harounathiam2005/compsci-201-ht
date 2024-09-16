4f3ee29e7eb091812e936cf2e68c005ec2f7586a
Harouna Thiam <harouna@Harounas-MacBook-Air.local>
2024-09-16 17:08:12 -0400

    began truespace

--- APT-2/Common.java
+++ APT-2/Common.java
@@ -5,23 +5,10 @@ public class Common {
     public int count (String a, String b) {
         HashMap<String, Integer> mapA = new HashMap<String, Integer>(0);
         HashMap<String, Integer> mapB = new HashMap<String, Integer>(0);
-        //ArrayList<String> returnChars = new ArrayList<String>(0);
         int count = 0;
         for (int i = 0; i < b.length(); i++) {
-            if (mapA.keySet().contains(b.substring(i,i+1))) {
-                mapA.replace(b.substring(i,i+1), mapA.get(b.substring(i,i+1))+1);
-            }
-            else {
-                mapA.put(b.substring(i,i+1),1);
-            }
-        }
-        for (int i = 0; i < a.length(); i++) {
-            if (mapB.keySet().contains(a.substring(i,i+1))) {
-                mapB.replace(a.substring(i,i+1), mapB.get(a.substring(i,i+1))+1);
-            }
-            else {
-                mapB.put(a.substring(i,i+1),1);
-            }
+            populate(mapB, b, i);
+            populate(mapA, a, i);
         }
         for (String c : mapA.keySet()) {
             for (String c2 : mapB.keySet()) {
@@ -33,5 +20,14 @@ public class Common {
             }
         }
         return count;
+    }
+
+    public static void populate(HashMap<String, Integer> map, String s, int index) {
+        if (map.keySet().contains(s.substring(index,index+1))) {
+            map.replace(s.substring(index,index+1), map.get(s.substring(index,index+1))+1);
+        }
+        else {
+            map.put(s.substring(index,index+1),1);
+        }
     }
  }
