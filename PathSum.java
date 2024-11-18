09d8e52d97b6f13b4d9ad6f53f601b77b875eb39
student <student@students-MacBook-Air-7.local>
2024-11-18 01:43:38 -0500

    APT 9

--- PathSum.java
+++ PathSum.java
@@ -1,8 +1,19 @@
-public class PathSum {
-    public int hasPath(int target, TreeNode tree){
-        if (tree.left != null || tree.right != null) {
-            
+public class PathSum 
+{
+    public int hasPath(int target, TreeNode tree) 
+    {
+        if (tree == null) 
+        {
+            return 0;
+        }
+        
+        if (tree.left == null && tree.right == null && tree.info == target) 
+        {
+            return 1;
         }
 
+        int targetedNum = target - tree.info;
+
+        return Math.max(hasPath(targetedNum, tree.left), hasPath(targetedNum, tree.right));
     }
 }
